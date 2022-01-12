9dff3b232a6c7da608fb31244d226440d5f58101
Vitor Crivelaro <v_crivelaro@hotmail.com>
2022-01-12 12:47:30 -0300

    Implantando Javascript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,19 @@
+/*
+Case Sensitive = reconhece letras maiusculas e minusculas
+
+por Tag: getElementByTagName()
+por id: getElementById()
+por nome: getElementsByName()
+por classe: getElementsByClassName()
+por seletor: querySelector()
+*/
+
 let nome = window.document.getElementById('nome')
+let email = window.document.querySelector ('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width='100%'
+email.style.width='100%'
 
 function mostraValor(){
     const limite = 30
@@ -8,5 +23,34 @@ function mostraValor(){
     if(restante < 30){
 
         vr.innerHTML = `Ainda faltam ${limite - restante} caracteres restantes.`
+    }
+}
+
+function validaNome(){
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3)
+    {
+        txtNome.innerHTML = 'Nome Inválido!'
+        txtNome.style.color = 'red'
+    }
+    else
+    {
+        txtNome.innerHTML = 'Nome Válido'
+        txtNome.style.color = 'green'
+    }
+}
+
+function validaEmail(){
+    let txtEmail= document.querySelector('#txtEmail')
+
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1)
+    {
+        txtEmail.innerHTML = 'Email Inválido'
+        txtEmail.style.color = 'red'
+    }
+    else
+    {
+        txtEmail.innerHTML = 'Email Válido'
+        txtEmail.style.color = "green"
     }
 }
